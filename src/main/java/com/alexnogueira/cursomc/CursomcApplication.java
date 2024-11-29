e27995b6d52c978a5a62f64b74de87c7a6d6546c
Alex Nogueira <alex.eustaquio.nogueira@gmail.com>
2024-11-29 05:48:27 -0300

    Criando operação de instanciação

--- src/main/java/com/alexnogueira/cursomc/CursomcApplication.java
+++ src/main/java/com/alexnogueira/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.alexnogueira.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.alexnogueira.cursomc.domain.Categoria;
+import com.alexnogueira.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Escritorio");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+
 	}
 
 }
